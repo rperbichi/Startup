f24bd5548d72531aa64f06b3b5e8e2ca149e1e59
Jaden Taylor <jtjadent@gmail.com>
2023-03-08 20:47:31 -0700

    sign up logo creates popup

--- script.js
+++ script.js
@@ -6,3 +6,14 @@
 // option to sign up
 
 // share button -> copies the link to the facebook group
+
+
+// const el = document.querySelector(".myclass");
+
+//document.querySelector('div[class="contact-logo"]').click(prompt("Enter email address"));
+//let ageQuestion = prompt("Enter email address");
+
+function promptMe(){
+    var userAdjective = prompt("Enter email address");
+    alert (userAdjective);
+}
