3a53a992d05106de8fd2efac5a2fa30d4324f06b
Raul Perbichi <raulperbichi@Perbichis-iMac.local>
2023-03-08 20:17:08 -0700

    Updates on JS file

---
+++ script.js
@@ -0,0 +1,8 @@
+// prayer request option under contact us tab:
+// https://mailtrap.io/blog/javascript-send-email/
+
+// weekly updates email
+
+// option to sign up
+
+// share button -> copies the link to the facebook group
