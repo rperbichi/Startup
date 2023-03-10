6c2e2451c03baea706e40e672edd9c5d442a34a1
RAUL PERBICHI <rperbichi97@gmail.com>
2023-03-10 16:33:28 -0700

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function login() {
     const nameEl = document.querySelector('#name');
     localStorage.setItem('userName', nameEl.value);
     window.location.href = 'prayer.html';
-} // Continue from here
+}
+
 
 
