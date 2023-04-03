c70ef1c6402ad1e212110f07f777c401471885a9
Raul Perbichi <rperbichi97@gmail.com>
2023-04-03 09:26:36 -0600

    Updating  endpointProxy.js

--- endpointProxy.js
+++ endpointProxy.js
@@ -3,22 +3,50 @@ const uuid = require('uuid'); // this will be used by database
 
 function endpointProxy(httpServer) {
     const wss = new WebSocketServer({noServer: true}); // we don't need to create a server
-    httpServer.on('upgrade', (reques, socket, head) => {
+    httpServer.on('upgrade', (request, socket, head) => {
         wss.handleUpgrade(request, socket, head, function done(ws) {
             wss.emit('connection', ws, request);
         });
     });
 
-    let prayerRequests = []; // creating variable to be updated
+    let prayerRequests = []; // creating variable to be updated by using the variable "connection" later on
 
     wss.on('connection', (ws) => {
-        const requestedPrayer = {id: uuid.v4(), alive: true, ws: ws}; // this will be used to store websocket 
-        prayerRequests.push(requestedPrayer); // adding each new prayer request to the list of prayer requests
+        const connection = {id: uuid.v4(), alive: true, ws: ws}; // this will be used to store websocket 
+        prayerRequests.push(connection); // adding each new prayer request to the list of prayer requests
+
+        wss.on('message', function message(data) { // this will be used to send a message that a prayer request has been submitted
+            prayerRequests.forEach((c) => { // going through each prayer requested currently stored in the prayerRequests variable
+                if (c.id !== connection.id){
+                    c.ws.send(data);
+                }
+            });
+        });
+        
+        wss.on('close', () => {
+            prayerRequests.findIndex((o, i) => { // o -> 
+                if(o.id === connection.id) {
+                    prayerRequests.splice(i, 1);
+                    return true;
+                }
+            });
+        });
+
+        wss.on('pong', () => { // pong messages: sent by a WS server to a WS client
+            connection.alive = true;
+        });
     });
 
-    wss.on('message', function message(data) { // this will be used to send a message that a prayer request has been submitted
-        prayerRequests.forEach((p) {
-            
-        })
-    })
+    setInterval(() => {
+        prayerRequests.forEach((c) => {
+            if(!c.alive) {
+                c.ws.terminate();
+            } else {
+                c.alive = false;
+                c.ws.ping();
+            }
+        });
+    }, 10000);
 }
+
+module.exports = {endpointProxy};
